2c091e7f798a3f995fcf8a8971077f0973bfad69
Kasthuri <kasthuri.14@cse.mrt.ac.lk>
2019-09-30 16:02:43 +0530

    effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter.reducer';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyCounterComponent } from './my-counter/my-counter.component';
+import { CounterEffects } from './counter.effects';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,7 @@ import { MyCounterComponent } from './my-counter/my-counter.component';
   imports: [
     BrowserModule,
     StoreModule.forRoot({ count: counterReducer }),
+    EffectsModule.forRoot([CounterEffects]),
     AppRoutingModule
   ],
   providers: [],
